71850a8ec858b5d7cc955b51bae9805559086b03
Maxim Frolov <buterbrodhs@gmail.com>
2021-01-20 22:19:01 +0300

    Правки UI, SEO

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -11,19 +11,24 @@ type Props = {
   home?: boolean
 }
 
-export const Layout = ({children, title = 'Frontend blog', home = false}: Props) => (
+export const Layout = ({children, title = 'Frontend blog.', home = false}: Props) => (
   <div className={styles.layoutContainer}>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8"/>
       <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+      <meta
+        name="keywords"
+        content="блог, фронтенд, разработка, janttaxan, javascript, frontend, дизайн, верстка, статьи, фролов, frolov"
+      />
+      <meta name="description" content="Конспекты, заметки и статьи на тему frontend разработки. Максим Фролов"/>
     </Head>
     <div className={styles.header}>
       <Header isHome={home}/>
     </div>
     {home &&
     <div className={styles.about}>
-        <About/>
+      <About/>
     </div>
     }
     <main>{children}</main>
